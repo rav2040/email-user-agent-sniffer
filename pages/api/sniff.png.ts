143473c0e8fbaec2b5665bb0475450332b720848
Stuart Kennedy <rav2040@gmail.com>
2022-06-04 14:04:14 +1200

    Change to aws sdk v2

--- pages/api/sniff.png.ts
+++ pages/api/sniff.png.ts
@@ -1,28 +1,36 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
-import { DynamoDBClient, PutItemCommand } from "@aws-sdk/client-dynamodb";
 import { createCanvas } from "canvas";
 import { nanoid } from "nanoid";
+
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  accessKeyId: process.env.V_ACCESS_KEY_ID,
+  secretAccessKey: process.env.V_SECRET_ACCESS_KEY,
+  region: "ap-southeast-2",
+});
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const response = await fetch("https://ipwho.is/" + req.socket.remoteAddress);
   const json = await response.json();
 
-  const client = new DynamoDBClient({ region: "ap-southeast-2" });
-  const command = new PutItemCommand({
-    TableName: "user-agents",
-    Item: {
-      id: { S: nanoid() },
-      timestamp: { S: new Date().toISOString() },
-      ip: { S: req.socket.remoteAddress ?? "" },
-      country_code: { S: json.success ? json.country_code : "" },
-      city: { S: json.success ? json.city : "" },
-      isp: { S: json.success ? json.connection.isp : "" },
-      user_agent_string: { S: req.headers["user-agent"] ?? "" },
-    },
-  });
-
-  client.send(command).then(console.log).catch(console.error);
+  new AWS.DynamoDB()
+    .putItem({
+      TableName: "user-agents",
+      Item: {
+        id: { S: nanoid() },
+        timestamp: { S: new Date().toISOString() },
+        ip: { S: req.socket.remoteAddress ?? "" },
+        country_code: { S: json.success ? json.country_code : "" },
+        city: { S: json.success ? json.city : "" },
+        isp: { S: json.success ? json.connection.isp : "" },
+        user_agent_string: { S: req.headers["user-agent"] ?? "" },
+      },
+    })
+    .promise()
+    .then(console.log)
+    .catch(console.error);
 
   const buf = createCanvas(1, 1).toBuffer("image/png");
   res.setHeader("content-type", "image/png");
